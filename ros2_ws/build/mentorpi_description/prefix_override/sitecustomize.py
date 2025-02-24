43dd00c8f71bcebbbdd99d791fc3132f25c64e92
juliusb828 <julius.konstantin.baur@gmail.com>
2025-02-24 09:52:56 +0100

    remove build, log and install folders and unused image

--- ros2_ws/build/mentorpi_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ro01/workspace/ros2_ws/install/mentorpi_description'
--- ros2_ws/build/orchestrator_launch/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ro01/workspace/ros2_ws/install/controller/share/controller/package.sh"
-. "/home/ro01/workspace/ros2_ws/install/peripherals/share/peripherals/package.sh"
